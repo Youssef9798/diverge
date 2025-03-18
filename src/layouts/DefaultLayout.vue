00a3a23187633a6a25a167eddb29d73a02b515bb
Youssef ElAsfoury <42498703+Youssef9798@users.noreply.github.com>
2025-03-18 20:21:30 +0200

    Build default layout

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router';
+
+</script>
+
+<template>
+  <div class="w-screen h-screen flex bg-gray-50 dark:bg-gray-700">
+    <RouterView />
+  </div>
+</template>
