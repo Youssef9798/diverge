afe0e41be68de0031c2ff611d79b4909b863769d
Youssef ElAsfoury <42498703+Youssef9798@users.noreply.github.com>
2025-03-18 20:21:43 +0200

    Build authenticated layout

---
+++ src/layouts/AuthenticatedLayout.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { RouterView } from 'vue-router'
+import { useI18n } from 'vue-i18n';
+
+import SideBar from '@/components/Layout/SideBar.vue';
+import NavBar from '@/components/Layout/NavBar.vue';
+
+const { locale } = useI18n({ useScope: 'global' });
+
+const currentLocaleDir = computed(() => {
+  return locale.value === 'ar' ? 'rtl' : 'ltr'
+})
+</script>
+
+<template>
+  <div :dir="currentLocaleDir" class="h-screen w-screen flex">
+    <SideBar />
+    <div class="flex-1 bg-gray-50 dark:bg-gray-700">
+      <div class="flex flex-col h-full overflow-y-auto px-8">
+        <div class="container mx-auto">
+          <NavBar />
+          <RouterView />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
